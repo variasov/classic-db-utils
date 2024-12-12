f10807d92b9d49c65b6e9b9311d7986321df7ce2
Pavel Lytkin <Pavel.Lytkin@evraz.com>
2024-12-12 12:27:41 +0300

    добавлен takes_connection

---
+++ sources/classic/db_utils/takes_connection.py
@@ -0,0 +1,23 @@
+from functools import wraps
+
+
+def takes_connection(connect_attr='connect', connection_param='connection'):
+    """Декоратор для автоматического получения соединения из атрибута объекта.
+
+    Args:
+        connect_attr: имя атрибута с методом connect
+        connection_param: имя параметра для передачи соединения
+    """
+
+    def decorator(func):
+
+        @wraps(func)
+        def wrapper(obj, *args, **kwargs):
+            connect = getattr(obj, connect_attr)
+            with connect() as conn:
+                kwargs[connection_param] = conn
+                return func(obj, *args, **kwargs)
+
+        return wrapper
+
+    return decorator
